70bb1a6ae811cf6034148603b63b5831d720ee38
Raphael dos Santos Jesus <raphaelsantos.j@gmail.com>
2017-06-20 16:19:29 -0300

    Teste 1

--- bot.js
+++ bot.js
@@ -14,28 +14,15 @@ module.exports = function()
   //   bot.sendMessage(chatId, resp);
   // });
 
-  // bot.onText(/\/meme (.+)/, (msg, match) => {
-
-  //   const chatId = msg.chat.id;
-  //   const mensagem = match[1];
-
-  //   giphy.search(mensagem, function (err, res) {
-  //     var meme = res.data[0].embed_url;
-  //     bot.sendMessage(chatId, meme);
-  //   });
-  // });
-
   bot.onText(/\/meme (.+)/, (msg, match) => {
 
     const chatId = msg.chat.id;
     const mensagem = match[1];
 
-      giphy.search(mensagem, function (err, res) {
-        var meme = res.data[0].embed_url;
-        console.log(meme);
-        bot.sendDocument(chatId, meme);
-        
-      })
+    giphy.search(mensagem, function (err, res) {
+      var meme = res.data[0].embed_url;
+      bot.sendMessage(chatId, meme);
+    });
   });
 
 }
