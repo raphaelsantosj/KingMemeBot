ffe4d2b8b7033ebb926019c2fb62c56743a79dd5
Raphael dos Santos Jesus <raphaelsantos.j@gmail.com>
2017-06-20 16:05:40 -0300

    add bot.js

---
+++ bot.js
@@ -0,0 +1,26 @@
+'use strict';
+const TelegramBot = require('node-telegram-bot-api');
+
+module.exports = function()
+{
+
+  // replace the value below with the Telegram token you receive from @BotFather
+  const token = '397219768:AAF4IBRdvLii3AZMT4p76PUcnNIKExCI_p8';
+
+  // Create a bot that uses 'polling' to fetch new updates
+  const bot = new TelegramBot(token, {polling: true});
+
+  // Matches "/echo [whatever]"
+  bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+  });
+
+}
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var http = require('http');
 
 var port = (process.env.PORT || 5000);
 
+require('./bot.js')();
+
 http.createServer(function(request, response) {
 	response.writeHead(200, {'Content-Type': 'application/json'});
 	response.write(JSON.stringify({name: 'kingmemebot', ver: '1.0'}));
